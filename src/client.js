a40817da9e055b1870519819777c45c51728c0d9
formago <formago.max@gmail.com>
2017-03-05 17:56:57 +0300

    end 2 part

--- src/client.js
+++ src/client.js
@@ -2,9 +2,16 @@
  import React      from 'react';
  import { browserHistory, Router } from 'react-router';
  import routes from './routes';
+  import { Provider } from 'react-redux';
+import configureStore from './redux/configureStore';
+
+const store = configureStore();
+
  const component = (
+    <Provider store={store}>
   <Router history={browserHistory}>
     {routes}
   </Router>
+   </Provider> 
  );
  ReactDOM.render(component, document.getElementById('react-view'));
